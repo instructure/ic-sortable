e76c60a6fe4bf4d998c46f0ac6d696e30475a905
Ryan Florence <rpflorence@gmail.com>
2014-04-05 23:38:04 -0600

    lib/sortable.js -> lib/main.js

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,7 @@ module.exports = function(broccoli) {
   return require('broccoli-dist-es6-module')(broccoli.makeTree('lib'), {
     global: 'ic.Sortable',
     packageName: 'ic-sortable',
-    main: 'sortable',
+    main: 'main',
     shim: {
       'ember': 'Ember',
       'ic-droppable': 'ic.Droppable.default'
